434746cfa3d0e8db473b89fbb546530d87547270
Jason Hardman <jason.hardman@churchofjesuschrist.org>
2023-07-05 15:40:28 -0600

    fixed issue with path to json file

--- all_the_ugly/json_to_dict.py
+++ all_the_ugly/json_to_dict.py
@@ -1,15 +1,32 @@
 # Categories
-'''This will turn the comma-separated lists in "THE_FILE" into a dictionary
+'''
+This will turn the comma-separated lists in "THE_FILE" into a dictionary
 where the first item is the key while the other items become a list as the
-value'''
+value
+
+The json file needs to be in the same directory as the main script that 
+references this script
+'''
 
 import sys
 import json
+import os
 
-THE_FILE = 'ursospecial.json'
+json_file_name  = 'ursospecial.json'
+path_2_script = os.path.abspath(sys.argv[0])
+path = os.path.dirname(path_2_script)
+THE_FILE = path + '/' + json_file_name
+output = '''
+script: {}
+Path to script: {}
+Just the path: {}
+Path to json file: {}
+'''
+print(output.format(sys.argv[0], path_2_script ,path, THE_FILE))
 
 try:
     with open(THE_FILE, 'r') as json_file:
         special_chars = json.load(json_file)
 except FileNotFoundError:
-    sys.exit('{} not found. Please refer to the README'.format(THE_FILE))
+    exit_msg = '{} not found. Please refer to the README'
+    sys.exit(exit_msg.format(THE_FILE))
